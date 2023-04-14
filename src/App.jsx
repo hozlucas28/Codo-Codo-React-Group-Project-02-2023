547e406881c163fc1ac2b0d7a01b537ebf10a81d
Lucas Hoz <hozlucas28@gmail.com>
2023-04-14 15:54:10 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { container, title, subTitle } from './App.module.css'
+
+function App() {
+	return (
+		<div className={container}>
+			<h1 className={title}>ViteJS Template</h1>
+			<h2 className={subTitle}>
+				<a
+					href='https://github.com/hozlucas28'
+					target='_blank'
+					rel='noreferrer'
+				>
+					@hozlucas28
+				</a>
+			</h2>
+		</div>
+	)
+}
+
+export default App
